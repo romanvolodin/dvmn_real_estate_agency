ea044a5ec01a26a4aa8adc1e6ec7b78edac67871
Roman Volodin <roman.volodin@gmail.com>
2021-09-21 10:59:27 +0300

    Make 'created_at' property read only in admin site

--- property/admin.py
+++ property/admin.py
@@ -5,5 +5,6 @@ from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['owner', 'town', 'address']
+    readonly_fields = ['created_at']
 
 admin.site.register(Flat, FlatAdmin)
