981f1faa7ece518146dd2a0110bceaed695d424c
Roman Volodin <roman.volodin@gmail.com>
2021-09-27 23:30:36 +0300

    Simplify Flat.new_building filling

--- property/migrations/0004_auto_20210921_1114.py
+++ property/migrations/0004_auto_20210921_1114.py
@@ -6,9 +6,7 @@ from django.db import migrations
 def fill_new_building_field(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     for flat in Flat.objects.all():
-        flat.new_building = False
-        if flat.construction_year >= 2015:
-            flat.new_building = True
+        flat.new_building = flat.construction_year >= 2015
         flat.save()
 
 
